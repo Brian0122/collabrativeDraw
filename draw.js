e4acfa9a9c5d8365807dfdf55f3a2b7f6ce1523b
Brian <kailinguy@gmail.com>
2013-09-15 17:22:06 +0800

    draw.js

--- draw.js
+++ draw.js
@@ -1,4 +1,5 @@
-var io = require('socket.io').listen();
+var port = process.env.PORT || 5000;
+var io = require('socket.io').listen(port);
 // assuming io is the Socket.IO server object
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
