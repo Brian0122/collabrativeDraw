6c6f3fd500c132c6de04b3d417d345867b1d91d6
Brian <kailinguy@gmail.com>
2013-09-15 16:58:28 +0800

    draw.js

--- draw.js
+++ draw.js
@@ -1,5 +1,9 @@
 var io = require('socket.io').listen(8000);
-
+// assuming io is the Socket.IO server object
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
 io.sockets.on('connection', function (socket) {
   
   socket.on('draw', function (data) {
